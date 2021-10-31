c18a0fc9f58bcea94e8330ba2c470cc755fff1df
Daniel Lopez <d.lopez.1740@gmail.com>
2021-10-31 19:07:43 -0300

    66. Signing a JWT Token on Sign In (Authentication)

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -3,23 +3,26 @@ import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
-    private usersRepository: UsersRepository
+    private usersRepository: UsersRepository,
+    private jwt: JwtService
   ) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn({ username, password }: AuthCredentialsDto): Promise<string> {
+  async signIn({ username, password }: AuthCredentialsDto): Promise<{ accessToken: string }> {
     const user = await this.usersRepository.findOne({ username });
 
     if (user && (await compare(password, user.password))) {
-      return 'success';
+      const accessToken = await this.jwt.signAsync({ username });
+      return { accessToken };
     }
 
     throw new UnauthorizedException('Please check your login credentials');

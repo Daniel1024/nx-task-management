ecd952ff24fc85948d6bd11d68247539be214430
Daniel Lopez <d.lopez.1740@gmail.com>
2021-10-31 18:43:06 -0300

    63. Feature: Signing In

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,15 @@ export class AuthService {
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn({ username, password }: AuthCredentialsDto): Promise<string> {
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await compare(password, user.password))) {
+      return 'success';
+    }
+
+    throw new UnauthorizedException('Please check your login credentials');
   }
 }

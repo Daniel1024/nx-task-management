1d7a2cd3fbde6e78683248f0258ba1f222d6b017
Daniel Lopez <d.lopez.1740@gmail.com>
2021-11-01 12:47:27 -0300

    74. Restricting Getting All Tasks

--- apps/api/src/app/tasks/tasks.repository.ts
+++ apps/api/src/app/tasks/tasks.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Brackets, EntityRepository, Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
@@ -7,17 +7,26 @@ import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  getTasks({ status, search }: GetTaskFilterDto): Promise<Task[]> {
+  getTasks({ status, search }: GetTaskFilterDto, user: User): Promise<Task[]> {
     const query = this.createQueryBuilder('task');
+
+    query.where({ user });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
 
     if (search) {
-      query.andWhere(
+      /*query.andWhere(
         'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
         { search: `%${search}%` }
+      );*/
+      query.andWhere(
+        new Brackets((qb) => {
+          qb.where('title ILIKE :search OR description ILIKE :search', {
+            search: `%${search}%`,
+          });
+        }),
       );
     }
 

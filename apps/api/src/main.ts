ae77647ad6f0a507cc489335551ff702567fb3c3
Daniel Lopez <d.lopez.1740@gmail.com>
2021-11-01 12:34:13 -0300

    73. Serialize User Data

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {

0bd5ff35094cc72a3179d498c19f5e70ecd0ddf5
Daniel Lopez <d.lopez.1740@gmail.com>
2021-11-01 12:55:32 -0300

    76. Restricting Getting a Task By ID

--- apps/api/src/app/tasks/tasks.controller.ts
+++ apps/api/src/app/tasks/tasks.controller.ts
@@ -22,8 +22,11 @@ export class TasksController {
   }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Promise<Task> {
-    return this.tasksService.getTaskById(id);
+  getTaskById(
+    @Param('id') id: string,
+    @GetUser() user: User
+  ): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
   }
 
   @Post()
@@ -39,11 +42,11 @@ export class TasksController {
     return this.tasksService.deleteTask(id);
   }
 
-  @Patch('/:id/status')
+  /*@Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
     @Body() { status }: UpdateTaskStatusDto
   ): Promise<Task> {
     return this.tasksService.updateTaskStatus(id, status);
-  }
+  }*/
 }
--- apps/api/src/app/tasks/tasks.service.ts
+++ apps/api/src/app/tasks/tasks.service.ts
@@ -18,8 +18,8 @@ export class TasksService {
     return this.tasksRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.tasksRepository.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.tasksRepository.findOne({ where: { id, user } });
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -39,10 +39,10 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+  /*async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
     const task = await this.getTaskById(id);
     task.status = status;
 
     return this.tasksRepository.save(task);
-  }
+  }*/
 }

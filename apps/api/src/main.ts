2dc0f51c33e0eaef17d109ccc80330641485a90f
Daniel Lopez <d.lopez.1740@gmail.com>
2021-11-02 10:57:56 -0300

    92. Front-end Application

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,16 +7,17 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const globalPrefix = 'api';
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalInterceptors(new TransformInterceptor());
 
   const port = process.env.PORT || 3333;
   await app.listen(port);
 
-  logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  logger.log(`Listening at http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
